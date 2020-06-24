794ca4319fbe11f559207741b52094e1323271fb
xpacheco <xpacheco@MARCOS>
2020-06-24 16:10:49 -0500

    Creacion de la clase cliente

--- PracticaDeLaboratorio05_PachecoM_PACHECOC/src/ec/edu/ups/modelo/Cliente.java
+++ PracticaDeLaboratorio05_PachecoM_PACHECOC/src/ec/edu/ups/modelo/Cliente.java
@@ -5,11 +5,110 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author xpacheco
  */
 public class Cliente {
     
+    //Atributos
+    private String cedula;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private List<Vehiculo> autos;
     
+    //Constructores
+
+    public Cliente() {
+    }
+
+    public Cliente(String cedula, String nombre, String direccion, String telefono, List<Vehiculo> autos) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.autos = autos;
+    }
+    
+    //Getters and setters
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Vehiculo> getAutos() {
+        return autos;
+    }
+
+    public void setAutos(List<Vehiculo> autos) {
+        this.autos = autos;
+    }
+    
+    
+    //Hashcode y Equals
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.cedula);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.cedula, other.cedula)) {
+            return false;
+        }
+        return true;
+    }
+    
+    // toString
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "cedula=" + cedula + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", autos=" + autos + '}';
+    }
+ 
 }
